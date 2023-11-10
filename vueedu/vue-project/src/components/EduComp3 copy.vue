73c0ceaa7cbfdba376fcf2bebcf183ca3441bb59
Your Name <jin0201948@gmail.com>
2023-11-10 19:20:48 +0900

    add vue.js

---
+++ vueedu/vue-project/src/components/EduComp3 copy.vue
@@ -0,0 +1,16 @@
+<template>
+	<div>
+		<button @click="increment()">Counter: {{ counter }}</button>
+	</div>
+</template>
+
+<script setup>
+import { onMounted, ref } from 'vue';
+
+		const counter = ref(0); // 내부적으로 반응형 객체로 만듦
+		const increment = () => counter.value++; // 메서드
+		onMounted(() => { // mount되면~ 실행해라~
+			console.log('애플리케이션이 마운트 되었습니다!');
+		});
+
+</script>
---
+++ vueedu/vue-project/src/pages/WorkView.vue
@@ -0,0 +1,45 @@
+<template>
+  <h2 style="color: #3f51b5">노은기의 Vue 실습 페이지</h2>
+  <br />
+  <div style="display: flex">
+    <TeamCard
+      name="문호준"
+      food="베이컨토마토디럭스버거"
+      srcUrl="images/moon.png"
+      teamNum="1"
+      @display-teamcard="displayTeamcard"
+      v-if="isShow"
+    ></TeamCard>
+    <TeamCard
+      name="노은기"
+      food="소금빵 까눌레"
+      srcUrl="images/noh.png"
+      teamNum="2"
+      @display-teamcard="displayTeamcard"
+      v-if="isShow"
+    ></TeamCard>
+    <TeamCard
+      name="손승범"
+      food="라멘 마라탕"
+      srcUrl="images/son.png"
+      teamNum="3"
+    ></TeamCard>
+    <TeamCard
+      name="이해성"
+      food="맥도날드 감자튀김"
+      srcUrl="images/sung.jpg"
+      teamNum="4"
+    ></TeamCard>
+  </div>
+</template>
+<script setup>
+import TeamCard from "@/components/TeamCard.vue";
+import { ref } from "vue";
+const isShow = ref(true);
+function displayTeamcard(data) {
+  
+  console.log(data);
+  console.log(isShow.value);
+  isShow.value = data;
+}
+</script>
